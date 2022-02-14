9bef5ef3e7af3c66e1b80f01588f2269535214e7
savinthie <99681266+savinthie@users.noreply.github.com>
2022-02-15 01:35:52 +0530

    Add files via upload

---
+++ tute3/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+    /* question 1.....
+        int count = 0;
+        do
+        {
+            System.out.print(" Hello ");
+            count = count + 1;
+        }while (count < 9);
+        System.out.println();
+
+question 2....
+int num1, num2;
+        Scanner read = new Scanner(System.in);
+        System.out.print("first number : ");
+        num1 = read.nextInt();
+        System.out.print("second number : ");
+        num2 = read.nextInt();
+        if (num1 <= num2) {
+            int i = num1 - 1;
+            int j = num2 - 1;
+            while (i <= j) {
+                System.out.println(i);
+                i++;
+            }
+
+
+        } else {
+            System.out.println("first number is greater than the second one...Try again!");
+        }
+
+question 3.....
+
+    int num;
+    String letter;
+    Scanner read=new Scanner(System.in);
+        System.out.print("Enter a number : ");
+    num= read.nextInt();
+        System.out.print("Enter a letter : ");
+    letter=read.next();
+    for (int i=1;i<=num;i++){
+        System.out.print(letter);
+    }
+*/
+
+}}
+
---
+++ tute3/q1.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class q1 {
+    public static void main(String[] args) {
+    int count = 0;
+        do
+    {
+        System.out.print(" Hello ");
+        count = count + 1;
+    }while (count < 9);
+        System.out.println();
+}}
